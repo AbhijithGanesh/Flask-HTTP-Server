92118482a610a4090bb929500d8cd739901ce41c
Abhijith Ganesh <g.abhijith14122003@gmail.com>
2021-08-29 00:23:55 +0530

    Changes to methods to accommodate other HTTP methods

--- app.py
+++ app.py
@@ -1,23 +1,43 @@
-from flask import Flask,render_template as render, abort, request, jsonify 
+from flask import Flask,render_template as render, request 
+from models import *
+
 
 
 app = Flask(__name__,template_folder='./templates')
 
+
 @app.route('/GETPage',methods = ['GET'])
 def Gpage():
-    return 'You have landed on the page which allows the GET methods'
+    return f"You have landed on the page which allows the {request.method} method."
 
 @app.route('/POSTPage', methods = ['GET','POST'])
 def Ppage():
     if request.method == "POST":
-        print(request.form)
-        '''
-        USE your model logic to write it to database
-        '''
+        WriteModelJson(dict(zip(dict(request.form).keys(), dict(request.form).values())))
         return render('SuccessPage.html')
     else:
         return render('form.html')
 
-@app.route('/MultiMethodPage', methods = ['GET','POST', 'PUT','PATCH', 'DELETE'])
+@app.route('/MultiMethodPage', methods = ['GET','PUT','PATCH', 'DELETE'])
 def MethodsPage():
-    return "Multiple Methods can be used here according to your python logic"
+    import json
+    if request.method == "DELETE":
+        data = json.loads(request.data)
+        message =  DeleteJsonModel(data)
+        return f"The Method used was: {request.method}\n  {message}"
+    
+    elif request.method == 'PUT':     
+        data = json.loads(request.data)
+        message = PutModelJson(data)
+        return f"The Method used was: {request.method}\n  {message}"
+
+    elif request.method == 'PATCH':
+        message = PatchModelJson(json.loads(request.data))
+        return f"The Method used was: {request.method}\n {message}"
+
+
+    
+    return "This page/URL supports PUT,PATCH and DELETE Methods. Use "
+
+if __name__ == "__main__":
+    app.run()
